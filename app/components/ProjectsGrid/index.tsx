b358290724a13e4ae5d61464bd39b2e1dc1f8f30
infinity0106 <berny.orozco@hotmail.com>
2023-10-23 20:46:52 -0700

    [SW-2] images

--- app/components/ProjectsGrid/index.tsx
+++ app/components/ProjectsGrid/index.tsx
@@ -28,9 +28,7 @@ export const ProjectsGrid = () => {
         <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
           <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
             <img
-              src={
-                'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg'
-              }
+              src={'/img/littlelemon.png'}
               alt=""
               className="h-full w-full object-cover object-center"
             />
@@ -38,18 +36,14 @@ export const ProjectsGrid = () => {
           <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
             <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
               <img
-                src={
-                  'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg'
-                }
+                src={'/img/apps_web.png'}
                 alt=""
                 className="h-full w-full object-cover object-center"
               />
             </div>
             <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
               <img
-                src={
-                  'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg'
-                }
+                src={'/img/invitacion_web.png'}
                 alt=""
                 className="h-full w-full object-cover object-center"
               />
@@ -57,9 +51,7 @@ export const ProjectsGrid = () => {
           </div>
           <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
             <img
-              src={
-                'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg'
-              }
+              src={'/img/padel_web.png'}
               alt=""
               className="h-full w-full object-cover object-center"
             />
