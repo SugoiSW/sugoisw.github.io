540871ef8e5e3bbf368d492fc74d580c28b8891a
infinity0106 <berny.orozco@hotmail.com>
2023-10-25 20:21:34 -0700

    [SW-2] metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,30 @@ import './global.css';
 
 export const metadata = {
   title: 'SugoiSW Soluciones',
-  description: 'Hecho con ❤️ por SugoiSW',
+  description:
+    'Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel',
+  keywords:
+    'Consulting, Software, Mobile Development, Web Development, Computer Vision, AI',
+  creator: 'SugoiSW',
+  authors: [{ name: 'bernardo' }],
+  publisher: 'SugoiSW',
+  openGraph: {
+    title: 'SugoiSW',
+    description:
+      'Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel',
+    url: 'https://sugoisw.github.io/',
+    siteName: 'SugoiSW',
+    images: [
+      {
+        url: 'https://sugoisw.github.io/img/logo.png',
+        width: 612,
+        height: 612,
+        alt: 'SugoiSW solutions',
+      },
+    ],
+    locale: 'es_MX',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -14,32 +37,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <title>SugoiSW</title>
-
         <link rel="SugoiSW" href="/favicon.ico" />
-        <meta
-          name="keywords"
-          content="Consulting, Software, Mobile Development, Web Development, Computer Vision, AI"
-        />
-        <meta
-          name="description"
-          content="Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel."
-        />
-
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <meta name="author" content="SugoiSW" />
-
-        <meta property="og:title" content="SugoiSW" />
-        <meta
-          property="og:description"
-          content="Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel."
-        />
-        <meta
-          property="og:image"
-          content="https://sugoisw.github.io/img/logo.webp"
-        />
-        <meta property="og:url" content="https://sugoisw.github.io/" />
       </Head>
       <body>{children}</body>
     </html>
