44eeaa786d7289c982d4f5f25d7b45b0a717247f
infinity0106 <berny.orozco@hotmail.com>
2023-10-27 20:32:50 -0700

    [SW-3] env local

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  env: {
+    NEXT_PUBLIC_MIXPANEL: '{NEXT_PUBLIC_MIXPANEL}',
+  },
 };
 
 const plugins = [
