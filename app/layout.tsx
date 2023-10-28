b766d961fc3a65995ce5e0c77b720452b2528a4e
infinity0106 <berny.orozco@hotmail.com>
2023-10-27 17:22:54 -0700

    [SW-3] fix export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,23 @@
 'use client';
-import Head from 'next/head';
 import './global.css';
 import { useEffect } from 'react';
 import mixpanel from 'mixpanel-browser';
-
-export const metadata = {
-  title: 'SugoiSW Soluciones',
-  description:
-    'Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel',
-  keywords:
-    'Consulting, Software, Mobile Development, Web Development, Computer Vision, AI',
-  creator: 'SugoiSW',
-  authors: [{ name: 'bernardo' }],
-  publisher: 'SugoiSW',
-  openGraph: {
-    title: 'SugoiSW',
-    description:
-      'Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel',
-    url: 'https://sugoisw.github.io/',
-    siteName: 'SugoiSW',
-    images: [
-      {
-        url: 'https://sugoisw.github.io/img/logo.png',
-        width: 612,
-        height: 612,
-        alt: 'SugoiSW solutions',
-      },
-    ],
-    locale: 'es_MX',
-    type: 'website',
-  },
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    // Initialize Mixpanel with your API token
-    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL || '', {
-      debug: true,
-      track_pageview: true,
-      persistence: 'localStorage',
-    });
-  }, []);
+  // useEffect(() => {
+  //   // Initialize Mixpanel with your API token
+  //   mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL || '', {
+  //     debug: true,
+  //     track_pageview: true,
+  //     persistence: 'localStorage',
+  //   });
+  // }, []);
   return (
     <html lang="en">
-      <Head>
-        <link rel="SugoiSW" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,34 @@ import { ContentSection } from './components/ContentSection';
 import { Footer } from './components/Footer';
 import { Nav } from './components/Nav';
 
+export const metadata = {
+  title: 'SugoiSW Soluciones',
+  description:
+    'Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel',
+  keywords:
+    'Consulting, Software, Mobile Development, Web Development, Computer Vision, AI',
+  creator: 'SugoiSW',
+  authors: [{ name: 'bernardo' }],
+  publisher: 'SugoiSW',
+  openGraph: {
+    title: 'SugoiSW',
+    description:
+      'Sugoisw es tu socio de confianza para soluciones de software de vanguardia. Nos especializamos en Desarrollo Móvil, Desarrollo Web, Visión por Computadora e Inteligencia Artificial, ofreciendo servicios innovadores y personalizados para ayudar a tu negocio a prosperar. Explora nuestra experiencia y déjanos llevar tu viaje digital al siguiente nivel',
+    url: 'https://sugoisw.github.io/',
+    siteName: 'SugoiSW',
+    images: [
+      {
+        url: 'https://sugoisw.github.io/img/logo.png',
+        width: 612,
+        height: 612,
+        alt: 'SugoiSW solutions',
+      },
+    ],
+    locale: 'es_MX',
+    type: 'website',
+  },
+};
+
 export default async function Index() {
   /*
    * Replace the elements below with your own.
